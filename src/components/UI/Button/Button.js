57490a63f12979b2fa5d13a72437576b334b1c3a
Taiene Yuki <taieneyamazaki@gmail.com>
2021-09-27 11:33:13 -0300

    feat: lib react-icon and google icons

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,10 @@
 import './button.scss';
 
-const Button = ({ variant, children, onClick, testid }) => {
+const Button = ({ variant, children, onClick, testid, icon, span, spanIcon }) => {
   const className = `button ${variant}`
   return (
     <button className={className} onClick={onClick} data-testid={testid}>
+      <span className={span}>{icon}</span>
       {children}
     </button>
   )
