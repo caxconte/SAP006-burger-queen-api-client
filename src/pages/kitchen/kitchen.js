703efe1433b93976370bc7f0e739d7678e44f913
Taiene Yuki <taieneyamazaki@gmail.com>
2021-10-03 14:00:14 -0300

    feat: set interval

--- src/pages/kitchen/kitchen.js
+++ src/pages/kitchen/kitchen.js
@@ -1,5 +1,6 @@
 import SideMenu from "../../components/side_menu/sidemenu";
 import OrderToDo from "../../components/pages/kitchen/toDo/todo";
+import { filterList, sortData } from "../../data";
 import { getOrders, updateOrders } from "../../services/data";
 import { useState, useEffect } from "react";
 
@@ -8,20 +9,25 @@ import "./kitchen.scss";
 function Kitchen() {
 
   useEffect(() => {
+    const interval = setInterval(() => {
     getAllOrders();
+    }, 2000);
+    return () => clearInterval(interval);
   }, [])
 
   function getAllOrders() {
     getOrders().then((list) => {
-      setPendingOrderList(
-        list.filter(
-          (item) => item.status === "pending")
-      );
-      setInProgressOrderList(
-        list.filter(
-          (item) => item.status === "in progress")
-      )
-    });
+      const filterPendingOrders = filterList(list, "status", "pending");
+      const sortPendingOrders = sortData(filterPendingOrders, "createdAt");  
+      setPendingOrderList(sortPendingOrders);
+
+      const filterInprogressOrders = filterList(list, "status", "in progress");
+      const sortInprogressOrders = sortData(filterInprogressOrders, "updatedAt");  
+      setInProgressOrderList(sortInprogressOrders);
+    })
+    .catch((error)=>{
+      alert(error);
+    })
   }
 
   const [inProgressOrderList, setInProgressOrderList] = useState([]);
@@ -30,16 +36,17 @@ function Kitchen() {
   function handleOrderStatusChange(orderId, status) {
     updateOrders(orderId, status)
       .then((order) => {
-        console.log("response", order)
-        getAllOrders();
+        if(order.code) {
+          alert(`${order.code}: ${order.message}`);
+        }
       })
       .catch((error) => {
         alert(`${error.code}: ${error.message}`);
       })
   }
 
-  useEffect(() => console.log("pending= ", pendingOrderList), [pendingOrderList]);
-  useEffect(() => console.log("inprogress= ", inProgressOrderList), [inProgressOrderList]);
+  // useEffect(() => console.log("pending= ", pendingOrderList), [pendingOrderList]);
+  // useEffect(() => console.log("inprogress= ", inProgressOrderList), [inProgressOrderList]);
 
   return (
     <>
